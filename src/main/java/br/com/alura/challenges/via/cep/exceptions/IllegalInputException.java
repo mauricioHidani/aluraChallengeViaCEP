6c4e7415cb26315d0e1a9f96ea80f8378b48f706
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-15 11:20:48 -0300

    feat: custom illegal input exception

---
+++ src/main/java/br/com/alura/challenges/via/cep/exceptions/IllegalInputException.java
@@ -0,0 +1,12 @@
+package br.com.alura.challenges.via.cep.exceptions;
+
+public class IllegalInputException extends RuntimeException {
+
+	public IllegalInputException() {
+	}
+
+	public IllegalInputException(String message) {
+		super(message);
+	}
+
+}
