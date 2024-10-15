284c9f40973ab1098618213c38cb9cc29328d628
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-15 11:21:12 -0300

    feat: custom json conversion exception

---
+++ src/main/java/br/com/alura/challenges/via/cep/exceptions/JsonConversionException.java
@@ -0,0 +1,9 @@
+package br.com.alura.challenges.via.cep.exceptions;
+
+public class JsonConversionException extends RuntimeException {
+
+	public JsonConversionException(String message) {
+		super(message);
+	}
+
+}
